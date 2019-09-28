96583b0ad98cc2bfa0abe3f2fbef18ab01ef1b91
sparshgahlot1 <sparshgahot0398@gmail.com>
2019-09-28 17:45:52 +0530

    MAin FUnction

---
+++ main.java
@@ -0,0 +1,15 @@
+class main
+{
+  public static void main(String [] args)
+  {
+    int a=4,b=2;
+    int c = addition.add(a,b);
+    int d = subtract.subtract(a,b);
+    int e = multiply.multiply(a,b);
+    int f = divide.divide(a,b);
+    System.out.println("addition = "+c);
+    System.out.println("subtraction = "+d);
+    System.out.println("multiplication = "+e);
+    System.out.println("division = "+f);
+  }
+}
