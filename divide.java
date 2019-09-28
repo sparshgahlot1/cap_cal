e92b699c9772669dc77e6bce704cf34ad9023e96
Hradyansh <hradyansh.parashar@st.niituniversity.in>
2019-09-28 17:55:23 +0530

    Divide & Subtract

---
+++ divide.java
@@ -0,0 +1,8 @@
+class divide
+{
+	public static int divide(int a,int b)
+	{
+	int m = a/b;
+	return m;
+	}
+}
---
+++ subtract.java
@@ -0,0 +1,8 @@
+class subtract
+{
+	public static int subtract(int a,int b)
+	{
+	int m = a-b;
+	return m;
+	}
+}
